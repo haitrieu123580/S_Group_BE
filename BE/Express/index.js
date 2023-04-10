0ddf23deef17eed8e9b4fdbf24246349262a9712
haitrieu123580 <haitrieu123580@gmail.com>
2023-04-10 15:12:16 +0700

    buoi 2

--- BE/Express/index.js
+++ BE/Express/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-
+// chuyen tat cac yeu cau bat dau /user sang userRouter
+const userRouter = require("./routers/User")
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const users =require('./User');
-// chuyen tat cac yeu cau bat dau /user sang userRouter
-const userRouter = require("./routers/User")
+
 app.use('/users',userRouter)
 
 
