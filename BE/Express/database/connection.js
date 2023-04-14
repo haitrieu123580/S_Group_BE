81cec1b44c73a2f9667463d48770391e563f9fe0
haitrieu123580 <haitrieu123580@gmail.com>
2023-04-14 16:09:06 +0700

    dotenv

--- BE/Express/database/connection.js
+++ BE/Express/database/connection.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "trieu61002",
-  port:'3306',
-  database:'nodejs'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 connection.connect(function(err) {
--- BE/Express/index.js
+++ BE/Express/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+require('dotenv').config();
 const app = express()
-const PORT = 3000
+const PORT =  process.env.PORT || 5000
 // chuyen tat cac yeu cau bat dau /user sang userRouter
 const userRouter = require("./routers/User")
 // Body Parser Middleware
