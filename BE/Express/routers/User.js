0cc55da7232f08d9b2f79126532e8605305dba59
haitrieu123580 <haitrieu123580@gmail.com>
2023-06-04 23:12:20 +0700

    fix: getuser w age

--- BE/Express/routers/User.js
+++ BE/Express/routers/User.js
@@ -31,29 +31,42 @@ userRouter.post('/createUser', [verifyToken, validateRegisterRequest], async (re
         return res.json({ message: 'not allowed' })
     }
 })
-// pagination
 userRouter.get('/getusers', async (req, res) => {
-    let page_size = req.query.page_size, page_index = req.query.page_index
-    var pagination = {};
-    if (page_index < 1) page_index = 1;
-    var offset = (page_index - 1) * page_size;
-    return Promise.all([
-        knex.count('* as count').from("users").first(),
-        knex.select("*").from("users").offset(offset).limit(page_size)
-    ]).then(([total, rows]) => {
-        var count = total.count;
-        var rows = rows;
-        pagination.total = count;
-        pagination.per_page = page_size;
-        pagination.offset = offset;
-        pagination.to = offset + rows.length;
-        pagination.last_page = Math.ceil(count / page_size);
-        pagination.current_page = page_index;
-        pagination.from = offset;
-        pagination.data = rows;
-        res.status(200).json({ message: pagination })
-    });
-})
+    let page_size = req.query.page_size || 10; // Kích thước trang mặc định là 10 nếu không được cung cấp
+    let page_index = req.query.page_index || 1; // Trang hiện tại mặc định là 1 nếu không được cung cấp
+    let age = req.query.age || null; // Giá trị tuổi tìm kiếm, mặc định là null nếu không được cung cấp
+  
+    let query = knex.select("id", "name", "age").from("users");
+    
+    // Tìm kiếm theo tuổi nếu giá trị tuổi được cung cấp
+    if (age) {
+      query = query.where("age", age);
+    }
+  
+    // Đếm tổng số bản ghi
+    let countQuery = knex.count("* as count").from("users").first();
+    let countResult = await countQuery;
+  
+    let count = countResult.count;
+    let offset = (page_index - 1) * page_size;
+  
+    // Lấy dữ liệu người dùng với phân trang và tìm kiếm theo tuổi
+    let dataQuery = query.offset(offset).limit(page_size);
+    let dataResult = await dataQuery;
+  
+    let rows = dataResult;
+    let pagination = {};
+    pagination.total = count;
+    pagination.per_page = page_size;
+    pagination.offset = offset;
+    pagination.to = offset + rows.length;
+    pagination.last_page = Math.ceil(count / page_size);
+    pagination.current_page = page_index;
+    pagination.from = offset;
+    pagination.data = rows;
+    res.status(200).json({ message: pagination });
+  });
+  
 //get user by id
 userRouter.get('/:id', verifyTokenAndAuthorization, async (req, res) => {
     const user = await knex.select().from('users').where('id', '=', req.user.id).first()
