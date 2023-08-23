94f66d4ae72e4343b925dd5e6a1096446a5cc702
haitrieu123580 <haitrieu123580@gmail.com>
2023-08-23 17:15:05 +0700

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,70 +1,117 @@
-const knex = require('../database/connection')
 const { hashedPassword } = require('../hash/hash');
 const User = require('../models/User.model');
 const UserRole = require('../models/UserRole.model')
+const multer = require('multer')
+// // fake-data
+// const fakeData = async(req, res) =>{
+//   try {
+//     for (i = 11; i<40; i++){
+//       const { salt, encryptedPassword } = await hashedPassword('password'); 
+//     await User.create({
+//       username: `user${i}`,
+//       password: encryptedPassword, 
+//       email: `user${i}@email.com`,
+//       gender: false,
+//       name: `user${i}`,
+//       age: 18,
+//       salt: salt,
+//     });
+
+//     }
+//     res.send('fake-data')
+
+//   } catch (error) {
+
+//   }
+// }
+//get all
+const getAll = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    return res.status(200).json({ data: users })
+  } catch (error) {
+
+  }
+}
+// upload Avatar
+const uploadAvatar = async (req, res) =>{
+  try {
+    
+  } catch (error) {
+    
+  }
+}
 // create new user by admin
 const createUser = async (req, res) => {
-    try {
-      console.log(req.body);
-      const { salt, encryptedPassword } = await hashedPassword(req.body.password); 
-      const [user, created] = await User.findOrCreate({
-        where: {
-          username: req.body.username,
-        },
-        defaults: {
-          username: req.body.username,
-          password: encryptedPassword, 
-          email: req.body.email,
-          gender: req.body.gender,
-          name: req.body.name,
-          age: parseInt(req.body.age),
-          salt: salt,
-          createdBy: req.user.id,
-        },
-      });
-      if (created) {
-        return res.status(200).json({ message: 'created new user successfully' });
-      } else {
-        return res.status(400).json({ message: 'user existed' });
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        message: 'Something wrong when creating new user',
-      });
+  try {
+    console.log(req.body);
+    const { salt, encryptedPassword } = await hashedPassword(req.body.password);
+    const [user, created] = await User.findOrCreate({
+      where: {
+        username: req.body.username,
+      },
+      defaults: {
+        username: req.body.username,
+        password: encryptedPassword,
+        email: req.body.email,
+        gender: req.body.gender,
+        name: req.body.name,
+        age: parseInt(req.body.age),
+        salt: salt,
+        createdBy: req.user.id,
+      },
+    });
+    if (created) {
+      return res.status(200).json({ data:{
+        id: user.id, 
+        username: user.username, 
+        email: user.email, 
+        gender: user.gender, 
+        age: user.age, 
+        createdBy: user.createdBy,
+        createdAt: user.createdAt
+      } });
+    } else {
+      return res.status(400).json({ message: 'user existed' });
     }
-  };
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: 'Something wrong when creating new user',
+    });
+  }
+};
 
 const getUsers = async (req, res) => {
-    let page_size = parseInt(req.query.page_size) || 10;
-    let page_index = parseInt(req.query.page_index) || 1;
-    let offset = (page_index - 1) * page_size;
-  
-    try {
-      let count = await User.count();
-      let users = await User.findAll({
-        attributes: ['id','username', 'name', 'age', 'email', 'createdBy', 'createdAt', 'gender'],
-        offset: offset,
-        limit: page_size,
-      });
-  
-      let pagination = {
-        total: count,
-        per_page: page_size,
-        offset: offset,
-        to: offset + users.length,
-        last_page: Math.ceil(count / page_size),
-        current_page: page_index,
-        from: offset,
-        users: users,
-      };
-  
-      res.status(200).json({ users});
-    } catch (error) {
-        console.log(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  };
+  let page_size = parseInt(req.query.page_size) || 10;
+  let page_index = parseInt(req.query.page_index) || 1;
+  let offset = (page_index - 1) * page_size;
+
+  try {
+    let count = await User.count();
+    let users = await User.findAll({
+      attributes: ['id', 'username', 'name', 'age', 'email', 'createdBy', 'createdAt', 'gender'],
+      offset: offset,
+      limit: page_size,
+    });
+
+    let pagination = {
+      total: count,
+      per_page: page_size,
+      offset: offset,
+      to: offset + users.length,
+      last_page: Math.ceil(count / page_size),
+      current_page: page_index,
+      from: offset,
+      users: users,
+    };
+
+    res.status(200).json({ users });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
 //get user by id
 const getUserById = async (req, res) => {
@@ -101,8 +148,9 @@ const updateUser = async (req, res) => {
     if (updatedRowsCount === 0) {
       return res.status(404).json({ message: 'User not found' });
     }
+
+    // Truy vấn lại thông tin người dùng sau khi cập nhật thành công
     const updatedUser = await User.findOne({
-      attributes: ['id', 'username', 'name', 'age', 'email', 'createdBy', 'createdAt', 'gender'],
       where: {
         id: userId
       }
@@ -114,6 +162,7 @@ const updateUser = async (req, res) => {
     return res.status(500).json({ message: 'Error' });
   }
 };
+
 
 
 // delete user
@@ -156,10 +205,11 @@ const assignRoleToUser = async (req, res) => {
   }
 }
 module.exports = {
-    createUser,
-    getUsers,
-    getUserById,
-    updateUser,
-    deleteUser,
-    assignRoleToUser
-  }
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  assignRoleToUser,
+  getAll
+}
